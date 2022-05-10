f7d01ddcf0d8b2c74c14d80e009f70c7008c06be
Stephan <StephanHC.gibson@gmail.com>
2022-05-10 14:05:56 +0200

    Add maximum bounces to the projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private int _speed = 100;
+    [SerializeField] private int _maxBounces = 3;
+    private int _bounceCount;
+
     public void SetDirection(Vector3 newDirection)
     {
         Rigidbody myRigidbody = GetComponent<Rigidbody>();
         myRigidbody.AddForce(newDirection * Time.deltaTime * _speed);
+    }
+
+    private void CheckCollisionCount()
+    {
+        if (_bounceCount >= _maxBounces) Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _bounceCount += 1;
+        CheckCollisionCount();
     }
 }
