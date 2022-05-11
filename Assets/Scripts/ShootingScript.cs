19b14b08aa9bfefd57035df12874affe1e8d73e8
NikolayP <nikipanovski@gmail.com>
2022-05-11 14:35:48 +0200

    Add catch projectile mechanic

--- Assets/Scripts/ShootingScript.cs
+++ Assets/Scripts/ShootingScript.cs
@@ -6,7 +6,12 @@ public class ShootingScript : MonoBehaviour
 {
     private Projectile heldProjectile;
     [SerializeField] private int _cooldownDuration = 1;
-    private bool _canFire = false;
+    private bool _canFire = false;      // currently equivalent to "is holding a projectile" (source irrelevant)
+
+    [Tooltip("Time before and after collision with a fired projectile in which the player can pick it up instead of getting hurt.")]
+    [SerializeField] private float bufferTime = 0.5f;
+    private float timeBetweenGrabAndCollision = 0f;
+    private bool isAttemptingGrab = false;
 
     private void SetCanFire(bool trueOrFalse)
     {
@@ -22,25 +27,92 @@ public class ShootingScript : MonoBehaviour
         heldProjectile = null;
     }
 
-    private void ShootingInput()
+    public void OnGrab(InputValue value)
     {
-        //input trigger through OnFire (technically this method is already useless)
-        if (_canFire) Fire();
+        if (isAttemptingGrab == false) isAttemptingGrab = true;
+        Debug.Log("Grab attempted.");
     }
 
     public void OnFire(InputValue value)
     {
-        ShootingInput();
+        if (_canFire) Fire();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ProjectilePickup") && _canFire == false)
         {
-            _canFire = true;
-            //Destroy(other.gameObject);
             pickProjectileUp(other.gameObject.GetComponent<Projectile>());     // dirty, should not be here
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Transform coll = collision.transform;
+
+        if (coll.CompareTag("ProjectilePickup"))
+        {
+            Projectile incomingProjectile = coll.gameObject.GetComponent<Projectile>();     // can fail if no Projectile script attached
+            if (isAttemptingGrab)   // can validly grab - the button was pressed up to bufferTime ago
+            {
+                pickProjectileUp(incomingProjectile);
+            }
+            else // is NOT attempting grab - await to be pressed up to bufferTime late
+            {
+                StartCoroutine(delayCollisionDamage(incomingProjectile));
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (isAttemptingGrab == true)
+        {
+            incrementTimer();
+            checkTimer();
+        }
+    }
+
+    private void incrementTimer()
+    {
+        timeBetweenGrabAndCollision += Time.deltaTime;
+    }
+
+    private bool checkTimer()
+    {
+        bool hasTimerElapsed = timeBetweenGrabAndCollision >= bufferTime;
+
+        if (hasTimerElapsed)
+        {
+            turnOffGrabState();
+        }
+
+        return hasTimerElapsed;
+    }
+
+    private void turnOffGrabState()
+    {
+        timeBetweenGrabAndCollision = 0.0f;
+        isAttemptingGrab = false;
+    }
+
+    private IEnumerator delayCollisionDamage(Projectile projectile)
+    {
+        while (!checkTimer())
+        {
+            incrementTimer();
+
+            if (isAttemptingGrab == true)
+            {
+                pickProjectileUp(projectile);
+                yield break;
+            }
+        }
+
+        //takeDamage();     // or addScore() or whatever equivalent fits
+        Debug.Log("ouch!");
+        Destroy(projectile.gameObject);    // dirty, I guess?
+                                           // Destroy(projectile) will only destroy the script and keep the ball alive forever! :)
     }
 
     private void pickProjectileUp(Projectile projectile)
@@ -48,5 +120,8 @@ public class ShootingScript : MonoBehaviour
         heldProjectile = projectile;
         heldProjectile.SetHoldingPlayer(this.gameObject);
         heldProjectile.SetState(ProjectileState.HELD);
+
+        _canFire = true;
+        Debug.Log("Grab successful.");
     }
 }
