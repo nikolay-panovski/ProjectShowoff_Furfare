2a70f777f1faa6fb1f7c4e41a8993ecf01033b0b
Stephan <StephanHC.gibson@gmail.com>
2022-05-27 14:23:34 +0200

    Create portals and improve stun/invincibility

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private Portal _linkedPortal;
+    [SerializeField] private float _cooldownDuration = 2;
+    private bool _active = true;
+
+    public void ToggleActive()
+    {
+        _active = !_active;
+        //Turns the portal back on after a cooldown
+        if (_active == false) Invoke("ToggleActive", _cooldownDuration);
+    }
+
+    public Vector3 GetLinkedPortalPosition()
+    {
+        Vector3 location = new Vector3(0, 0, 0);
+        if (_linkedPortal != null) location = _linkedPortal.transform.position;
+        return location;
+    }
+
+    public bool GetActiveStatus()
+    {
+        return _active;
+    }
+
+    public Portal GetLinkedPortal()
+    {
+        return _linkedPortal;
+    }
+}
