ef6a16f9f7b3e3a2e26ff262405fd6f22d597d93
NikolayP <nikipanovski@gmail.com>
2022-06-02 13:06:02 +0200

    Add PlayerConfig script for player info on char select

--- Assets/Scripts/PlayerConfig.cs
+++ Assets/Scripts/PlayerConfig.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerConfig : MonoBehaviour
+public class PlayerConfig
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public PlayerInput input { get; set; }
+    public int playerIndex { get; set; }
+    public bool isReady { get; set; }
+    public Sprite characterSprite { get; set; }
 }
