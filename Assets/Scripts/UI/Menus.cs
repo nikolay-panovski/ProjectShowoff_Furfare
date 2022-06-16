f1e199a0b9e427465adf400941c7d1e48e7865a5
Data <lomadze.data@gmail.com>
2022-06-16 15:42:50 +0400

    Revert "Add the first version of a working UI"
    This reverts commit 184b5b35b0c6af37037cfc918382d20aace21e89.

--- Assets/Scripts/UI/Menus.cs
+++ Assets/Scripts/UI/Menus.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class Menus : MonoBehaviour
 {
-    public GameObject MainMenu;
     public GameObject CharacterSelection;
     public GameObject Infographic;
-    public GameObject Settings;
     public void CharacterSelectionOn()
     {
         CharacterSelection.gameObject.SetActive(true);
     }
-    public void SettingsOn()
-    {
-        MainMenu.gameObject.SetActive(false);
-        Settings.gameObject.SetActive(true);
-    }
     public void Controls()
     {
-        Settings.gameObject.SetActive(false);
         Infographic.gameObject.SetActive(true);
     }
     public void Quit()
